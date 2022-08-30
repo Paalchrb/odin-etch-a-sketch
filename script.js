3f9dcb8e9bfde480e940e08a543032b5d317a6d5
pal-christianby <pal.christian.by@ntb.no>
2022-08-30 14:58:56 +0200

    Create functionality which generate a grid with the provided number of cells

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log("hello world!");
+const NUMBER_OF_COLUMNS = 32;
+
+/**
+ * @function createGrid
+ * @description Creates a grid with columns x columns pixels
+ * @param {number} columns
+ */
+function createGrid(columns) {
+  const container = document.querySelector("div.container");
+
+  // create rows
+  for(let i = 0; i < columns; i++) {
+    const row = document.createElement("div");
+    row.className = "row";
+  
+    // create and append row-items
+    for (let j = 0; j < columns; j++) {
+      const rowItem = document.createElement("div");
+      rowItem.className = "row-item";
+      row.appendChild(rowItem);
+    }
+  
+    // append row to grid container
+    container.appendChild(row);
+  }
+}
+
+createGrid(NUMBER_OF_COLUMNS);
